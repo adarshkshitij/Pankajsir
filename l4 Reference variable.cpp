49e72993d9eb51a9a8d4fd2adf0970cdaa853e02
Adarsh <adarshandadarsh24@gmail.com>
2026-04-13 18:23:43 +0530

    Add initial implementation of reference variable examples in C++

---
+++ l4 Reference variable.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+// {
+//     int a=10;
+//     int &b=a; // reference variable
+//     cout<<"Value of a is "<<a<<endl;
+//     cout<<"Value of b is "<<b<<endl;
+//     b=20; // changing value of b will change value of a
+//     cout<<"Value of a is "<<a<<endl;
+//     cout<<"Value of b is "<<b<<endl;
+//     return 0;
+// }
+// no physical memory location is provided to the y 
+
+
+// {
+// int &x=10;
+// int i = 2, j=3;
+// int &y = i+j;
+// double d = 9.8;
+// int &z=d;
+// return 0;
+// }
+
